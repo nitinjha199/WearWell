3f41039b16c01a75fa1c18b8283861910f3945ac
nitinjha199 <nitinkumarjha199@gmail.com>
2025-08-27 00:35:06 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -110,7 +110,7 @@ export const adminLogin = async (req,res) => {
         res.cookie("token",token,{
         httpOnly:true,
         secure:true,
-        sameSite: none",
+        sameSite: "none",
         maxAge: 1 * 24 * 60 * 60 * 1000
     })
     return res.status(200).json(token)
